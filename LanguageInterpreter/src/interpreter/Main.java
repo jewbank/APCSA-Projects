a3df8aa319b7b79336b65489a52815a40967411e
jewbank <54724686+jewbank@users.noreply.github.com>
2019-10-03 11:43:11 -0500

    Added LanguageInterpreter Starter Code

---
+++ LanguageInterpreter/src/interpreter/Main.java
@@ -0,0 +1,32 @@
+package interpreter;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Lexicon lex = new Lexicon();
+
+    String newWord = lex.translate("happy");
+    System.out.println(newWord);
+
+  }
+
+  /**
+   * This method decodes a single word.
+   * 
+   * @param word The single word to be decoded.
+   */
+  public static String decode(String word)
+  {
+    return "";
+  }
+
+  /**
+   * This method should not have code copy/pasted from the decode method.
+   * 
+   * @param message The sentence (multiple words) to be decoded.
+   */
+  public static String decodeMessage(String message)
+  {
+    return "";
+  }
+}
